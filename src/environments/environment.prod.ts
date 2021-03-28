3108635c37ac74e65db63d9837731d4ded738061
LROMERO8 <lromero_rsx@hotmail.com>
2021-03-27 22:34:16 -0400

    adding mapbox token to environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoibHJvbWVybzgiLCJhIjoiY2ttbGZpMDRlMDU0YzJ2bnhhMGV3MXk4dCJ9.erq_VeKqSSG1ifnvZjwP7Q'
+  }
 };
